5f4b7292f6e7b66e7e4afe2c0b4da1e793436486
marina-1205 <marina.1205panda@gmail.com>
2023-08-15 03:07:47 +0000

    Complete customer index page

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,31 @@
 class Admin::CustomersController < ApplicationController
-  def edit
-  end
 
   def index
+    @customers = Customer.all.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "会員情報が更新されました。"
+      redirect_to admin_customer_path(@customer)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_kana_name, :last_kana_name, :email, :address, :phone_number, :is_deleted)
+  end
+
 end
