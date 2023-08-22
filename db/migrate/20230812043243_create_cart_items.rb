f814c3895b0acdce11e159be8774e80f45d93d69
marina-1205 <marina.1205panda@gmail.com>
2023-08-22 15:32:37 +0000

    integer2

--- db/migrate/20230812043243_create_cart_items.rb
+++ db/migrate/20230812043243_create_cart_items.rb
@@ -2,7 +2,7 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
       
-      t.integer :customer_id, null: false, foreign_key: true
+      t.integer :customer_id, null: false
       t.references :item, null: false, foreign_key: true
       t.integer :quantity, null: false
 
