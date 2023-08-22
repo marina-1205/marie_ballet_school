95af51d1866b93660a36d01f181e2ce36de0774e
marina-1205 <marina.1205panda@gmail.com>
2023-08-22 14:58:47 +0000

    cart_item

--- db/migrate/20230822135454_change_datatype_customer_id_of_cart_items.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeCustomerIdOfCartItems < ActiveRecord::Migration[6.1]
-  def change
-    change_column :cart_items, :customer_id, :bigint, null: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_22_135454) do
+ActiveRecord::Schema.define(version: 2023_08_21_102224) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,9 +53,9 @@ ActiveRecord::Schema.define(version: 2023_08_22_135454) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "quantity", null: false
-    t.bigint "customer_id", null: false
+    t.integer "customer_id", null: false
     t.integer "item_id", null: false
+    t.integer "quantity", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customer_id"], name: "index_cart_items_on_customer_id"
