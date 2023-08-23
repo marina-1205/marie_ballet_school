a2143fc689f07fa8ee618739a36f8d27f4519b40
marina-1205 <marina.1205panda@gmail.com>
2023-08-23 11:35:50 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-#    Admin.create!(
-#    email: 'marie@mail',
-#    password: '202306'
-#    )
-# #二回目rails db:seedするときは、元々登録してた記述をコメントアウトする
+    Admin.create!(
+    email: 'marie@mail',
+    password: '202306'
+    )
+#二回目rails db:seedするときは、元々登録してた記述をコメントアウトする
 
-# #ジャンルカラム記述
-#    Genre.create!(name: 'プリンセス')
-#    Genre.create!(name: '村娘')
-#    Genre.create!(name: '王子様')
-#    Genre.create!(name: 'ソリスト')
+#ジャンルカラム記述
+    Genre.create!(name: 'プリンセス')
+    Genre.create!(name: '村娘')
+    Genre.create!(name: '王子様')
+    Genre.create!(name: 'ソリスト')
 
-# #レッスンクラスカラム記述
-#    LessonClass.create!(name: 'アムールクラス')
-#    LessonClass.create!(name: 'フルールクラス')
-#    LessonClass.create!(name: 'ルミエールクラス')
-#    LessonClass.create!(name: 'オーロラクラス')
-#    LessonClass.create!(name: 'ダイヤモンドクラス')
-#    LessonClass.create!(name: 'グレースクラス')
-#    LessonClass.create!(name: 'エレガンスクラス')
+#レッスンクラスカラム記述
+    LessonClass.create!(name: 'アムールクラス')
+    LessonClass.create!(name: 'フルールクラス')
+    LessonClass.create!(name: 'ルミエールクラス')
+    LessonClass.create!(name: 'オーロラクラス')
+    LessonClass.create!(name: 'ダイヤモンドクラス')
+    LessonClass.create!(name: 'グレースクラス')
+    LessonClass.create!(name: 'エレガンスクラス')
