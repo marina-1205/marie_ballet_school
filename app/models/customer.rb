cf253a8d5bf3b0fbe59ef8fb919e641863878e9e
marina-1205 <marina.1205panda@gmail.com>
2023-10-01 08:13:20 +0000

    Validation improvement

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -21,13 +21,13 @@ class Customer < ApplicationRecord
   end
 
   validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_kana_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "フリガナ（名前）はカタカナのみである必要があります" }
+  validates :last_kana_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "フリガナ（苗字）はカタカナのみである必要があります" }
   validates :first_kana_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "メールアドレスは半角文字のみである必要があります" }
   validates :address, presence: true
-  validates :postcode, presence: true
+  validates :postcode, presence: true, format: { with: /\A\d+\z/, message: "郵便番号は数字のみである必要があります" }
   validates :school_name, presence: true
   validates :post_name, presence: true
-  validates :phone_number, presence: true
+  validates :phone_number, presence: true, format: { with: /\A\d+\z/, message: "電話番号は数字のみである必要があります" }
 end
--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -5,10 +5,10 @@ class Reserve < ApplicationRecord
    
    validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_kana_name, presence: true
-  validates :first_kana_name, presence: true
-  validates :phone_number, presence: true
-  validates :email, presence: true
+  validates :last_kana_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "フリガナ（苗字）はカタカナのみである必要があります" }
+  validates :first_kana_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "フリガナ（名前）はカタカナのみである必要があります" }
+  validates :phone_number, presence: true, format: { with: /\A\d+\z/, message: "電話番号は数字のみである必要があります" }
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "メールアドレスは半角文字のみである必要があります" }
   validates :age, presence: true
   validates :reservation_date, presence: true
   
